cfd4227e50d091051d6dc00719d6e0ce070bacd2
Aitor C. Figueroa <aitorcf@gmail.com>
2022-04-21 09:22:55 +0200

    Changes portfolio

---
+++ build/precache-manifest.88fa8d38a473664fb498c720113399bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "433dfdcf15d4068e6e4d",
+    "url": "/static/css/main.eade022b.chunk.css"
+  },
+  {
+    "revision": "433dfdcf15d4068e6e4d",
+    "url": "/static/js/main.433dfdcf.chunk.js"
+  },
+  {
+    "revision": "ca9df0cad693df686e81",
+    "url": "/static/js/1.ca9df0ca.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d07776a2461877e5723c8a187f83c4ea",
+    "url": "/static/media/space.d07776a2.jpg"
+  },
+  {
+    "revision": "7e1c938ee3191d256f761b01b3a53ec7",
+    "url": "/index.html"
+  }
+];
